30a50dbcc7d46e63d8cfaf22b982cba1f9079a07
beans3142 <greenbeans3142@naver.com>
2026-09-17 03:51:33 +0900

    feat(config): serve config from local filesystem, not GitHub

---
+++ config-server/src/test/java/com/wangy/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,39 @@
+package com.wangy.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("native")
+public class ConfigServerApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void servesEurekaServerConfigFromLocalFileSystem() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/eureka-server/default", String.class);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertTrue(response.getBody().contains("eureka-server"));
+    }
+}
